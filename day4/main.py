234cac3c4c2de8d3841adf55afe7e959965db180
Bruno Wu <brunowu2000@gmail.com>
2022-12-17 18:14:18 +0000

    day4

--- day4/main.py
+++ day4/main.py
@@ -8,7 +8,7 @@ def main():
     for pair in pairs:
         fst, snd = pair.split(',')
         fst_l, fst_r = fst.split('-')
-        snd_l, snd_r = snd.split('-') 
+        snd_l, snd_r = snd.split('-')
         fst_l, fst_r, snd_l, snd_r = int(fst_l), int(fst_r), int(snd_l), int(snd_r)
 
         if fst_l <= snd_l and fst_r >= snd_r:
@@ -26,13 +26,15 @@ def main2():
     for pair in pairs:
         fst, snd = pair.split(',')
         fst_l, fst_r = fst.split('-')
-        snd_l, snd_r = snd.split('-') 
+        snd_l, snd_r = snd.split('-')
         fst_l, fst_r, snd_l, snd_r = int(fst_l), int(fst_r), int(snd_l), int(snd_r)
 
-        if fst_l <= snd_r or snd_l <= fst_r:
+        if (fst_l <= snd_r and fst_r >= snd_l) or (snd_l <= fst_r and fst_l <= snd_r):
             total += 1
+
 
     return total
 
-print(main())
+# print(main())
 print(main2())
+# print(main3())
